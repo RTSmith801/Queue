9d94623b257405d0b14a59ed5e1358e0e674333a
Wreckeshey <RTSmith801@gmail.com>
2022-12-30 13:56:45 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/**
+ * @file: Queue.h
+ * @assignment_name:  04 Try it out - Queue
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 12/30/2022
+ * @brief: Using the queue data structure to display understanding of linked structures.
+ *
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
